95939f5c790e1c11540ce999b08612a09e0ae1cd
Justin Herman <Justin6@gmail.com>
2015-07-20 12:23:40 -0400

    Create norm-mux.bat

---
+++ norm-mux.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM Two commands to normalize and mux audio.
+REM uses https://github.com/slhck/audio-normalize and ffmpeg
+
+normalize.py -v -i %1
+
+ffmpeg -i %1 -i normalized-%~n1.wav -c copy -map 0:v:0 -map 1:a:0 %~n1.mkv
+
